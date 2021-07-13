9afc75dab4e82b66a80d3bdccfbdd4eb4dd902c5
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-07-13 19:10:06 +0100

    server added

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+const db = mongoose.connect();
+
+// routes
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = db;
